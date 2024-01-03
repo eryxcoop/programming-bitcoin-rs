d10516b3ef5b326d8afea23e7da0037e8f0073b6
Sergio Chouhy <sergio.chouhy@gmail.com>
2024-01-03 07:35:38 -0300

    refactor sign to take bytes as message and private key

--- src/signature.rs
+++ src/signature.rs
@@ -1,8 +1,12 @@
 use crate::{
+    hash::hash256,
     random::IsRandomScalarGenerator,
     secp256k1::{Secp256k1, Secp256k1ScalarFelt},
 };
-use lambdaworks_math::{cyclic_group::IsGroup, elliptic_curve::traits::IsEllipticCurve};
+use lambdaworks_math::{
+    cyclic_group::IsGroup, elliptic_curve::traits::IsEllipticCurve, traits::ByteConversion,
+    unsigned_integer::element::U256,
+};
 
 pub(crate) struct EllipticCurveDigitalSignatureAlgorithm;
 
@@ -11,6 +15,7 @@ pub(crate) struct ECDSASignature {
     r: Secp256k1ScalarFelt,
     s: Secp256k1ScalarFelt,
 }
+pub(crate) type PrivateKey = [u8; 32];
 
 pub(crate) struct RandomScalarGenerator;
 
@@ -22,25 +27,26 @@ impl ECDSASignature {
 
 impl EllipticCurveDigitalSignatureAlgorithm {
     fn sign(
-        z: Secp256k1ScalarFelt,
-        private_key: Secp256k1ScalarFelt,
+        z: &[u8],
+        private_key: PrivateKey,
         random: &mut impl IsRandomScalarGenerator,
     ) -> ECDSASignature {
+        let z = Secp256k1ScalarFelt::new(U256::from_bytes_be(&hash256(z)).unwrap());
+        let e = Secp256k1ScalarFelt::new(U256::from_bytes_be(&private_key).unwrap());
+
         let k = random.random_scalar();
         let R = Secp256k1::generator()
             .operate_with_self(k.representative())
             .to_affine();
         let r = Secp256k1ScalarFelt::new(R.x().representative());
         let k_inv = k.inv().unwrap();
-        let s = (z + private_key * &r) * k_inv;
+        let s = (z + e * &r) * k_inv;
         ECDSASignature::new(r, s)
     }
 }
 
 #[cfg(test)]
 pub mod tests {
-    use lambdaworks_math::{traits::ByteConversion, unsigned_integer::element::U256};
-
     use crate::{
         hash::hash256,
         secp256k1::Secp256k1ScalarFelt,
@@ -58,14 +64,11 @@ pub mod tests {
 
     #[test]
     fn test_signature() {
-        let private_key = Secp256k1ScalarFelt::new(
-            U256::from_bytes_be(&hash256("my secret".as_bytes())).unwrap(),
-        );
-        let z = Secp256k1ScalarFelt::new(
-            U256::from_bytes_be(&hash256("my message".as_bytes())).unwrap(),
-        );
+        let private_key = hash256("my secret".as_bytes());
+        let z = "my message".as_bytes();
 
         let signature = ECDSA::sign(z, private_key, &mut TestRandomScalarGenerator);
+
         let signature_expected = ECDSASignature::new(
             Secp256k1ScalarFelt::from_hex_unchecked(
                 "2b698a0f0a4041b77e63488ad48c23e8e8838dd1fb7520408b121697b782ef22",

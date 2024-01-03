1b7f5acd560f40c8a803b83df50bbaa829941558
Sergio Chouhy <sergio.chouhy@gmail.com>
2024-01-03 07:13:30 -0300

    remove vecs from hash fns

--- src/hash.rs
+++ src/hash.rs
@@ -1,13 +1,13 @@
 use sha2::{Digest, Sha256};
 
-pub(crate) fn sha256(data: &[u8]) -> Vec<u8> {
+pub(crate) fn sha256(data: &[u8]) -> [u8; 32] {
     let mut hasher = Sha256::new();
     hasher.update(data);
     let e: [u8; 32] = hasher.finalize().into();
     e.into()
 }
 
-pub(crate) fn hash256(data: &[u8]) -> Vec<u8> {
+pub(crate) fn hash256(data: &[u8]) -> [u8; 32] {
     sha256(&sha256(data))
 }
 
@@ -20,7 +20,7 @@ pub mod tests {
         let z = sha256("".as_bytes());
 
         // z_expected = 0xe3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
-        let z_expected = vec![
+        let z_expected = [
             227, 176, 196, 66, 152, 252, 28, 20, 154, 251, 244, 200, 153, 111, 185, 36, 39, 174,
             65, 228, 100, 155, 147, 76, 164, 149, 153, 27, 120, 82, 184, 85,
         ];
@@ -32,7 +32,7 @@ pub mod tests {
         let z = hash256("my message".as_bytes());
 
         // z_expected = 0x231c6f3d980a6b0fb7152f85cee7eb52bf92433d9919b9c5218cb08e79cce78
-        let z_expected = vec![
+        let z_expected = [
             2, 49, 198, 243, 217, 128, 166, 176, 251, 113, 82, 248, 92, 238, 126, 181, 43, 249, 36,
             51, 217, 145, 155, 156, 82, 24, 203, 8, 231, 156, 206, 120,
         ];
